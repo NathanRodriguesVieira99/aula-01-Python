0c61406ce537d16a9a36c302a933edb46d44f0df
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-13 09:01:04 -0300

    feat:  conditionals

---
+++ aula-02/main.py
@@ -0,0 +1,11 @@
+num1 = int(input('Informe o primeiro número: '))
+num2 = int(input('Informe o segundo número: '))
+
+
+if num1 > num2:
+    print('O primeiro número é maior que o segundo')
+elif num1 < num2:
+    print('O primeiro número é menor que o segundo')
+else:
+    print('Os números são iguais')
+
